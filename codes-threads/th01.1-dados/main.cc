78496c02f1972eaba5bdae77e80c6d0a52de2dde
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2025-09-28 21:19:49 -0300

    update

--- codes-threads/th01.1-dados/main.cc
+++ codes-threads/th01.1-dados/main.cc
@@ -1,24 +1,19 @@
 #include <global.hh>
 
-void rollDice(int id, int numMax){
+void tirarDado(int id, int numMax){
 	std::mt19937 mt(std::random_device{}());
 	std::uniform_int_distribution<int> dist(1, numMax); 
-	int value = dist(mt);
+	int valor = dist(mt);
 	
-	std::cout << "Dice " << id << ": roll\n";
-	std::cout << "Dice " << id << " value: " << value << "\n";
+	std::cout << "Dado " << id << ", valor: " << valor << "\n";
 	
 }
 
 
 int main(int argc, char* argv[]){
 
-	std::cout << "Total threads = " <<
-		std::thread::hardware_concurrency() << 
-		std::endl;
-	
-	std::thread t01(rollDice, 0, 6);
-	std::thread t02(rollDice, 1, 6);
+	std::thread t01(tirarDado, 0, 6);
+	std::thread t02(tirarDado, 1, 6);
 	
 	t01.join();
 	t02.join();
