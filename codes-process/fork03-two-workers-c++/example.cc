f42390c9e19009f96303c809e4a884fe686b732f
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2025-10-01 10:26:18 -0300

    update workers

--- codes-process/fork03-two-workers-c++/example.cc
+++ codes-process/fork03-two-workers-c++/example.cc
@@ -3,22 +3,26 @@
 #include <unistd.h>
 #include <random>
 
-void childMain(){
+void lanzarDado(){
     pid_t miPID = getpid();
     pid_t parentPID = getppid();
 
     std::mt19937 mt(std::random_device{}());
-    std::uniform_int_distribution<int> dist(1, 5); 
-    int workSimTime = dist(mt);  // Obtener un número aleatorio
+    std::uniform_int_distribution<int> dist(1, 10); 
+    int tiempoLanzamiento = dist(mt);  // Obtener un número aleatorio
+
+    std::uniform_int_distribution<int> valor(1, 6); 
+    int valorDado = valor(mt);  // Obtener un número aleatorio
 
 
     std::cout << ">> Proceso Hijo PID:" << miPID << ". Proceso Padre PID: " << parentPID << "\n";
-    std::cout << ">> Proceso Hijo PID:" << miPID << ". workSimTime: " << workSimTime << "\n";
+    std::cout << ">> Proceso Hijo PID:" << miPID << ". Tiempo  de Lanzamiento: " << tiempoLanzamiento << "\n";
 
     // Simular carga de trabajo
-    sleep(workSimTime); 
+    sleep(tiempoLanzamiento); 
 
     parentPID = getppid();
+    std::cout << "<< Proceso Hijo PID:" << miPID << ". Valor dado: " << valorDado << "\n";
     std::cout << "<< Proceso Hijo PID:" << miPID << ". Proceso Padre PID: " << parentPID << "\n";
 
     exit(EXIT_SUCCESS);
@@ -38,14 +42,14 @@ int main(int argc, char* argv[]) {
             std::cout << "Proceso Padre PID:" << miPID <<". Proceso hijo creado con PID: " << pid2 << "\n";   
         }
         else if (pid2 == 0) {  // Código EXCLUSIVO del proceso hijo
-            childMain();
+            lanzarDado();
         }
 
         wait(&returnCode);  // Esperar a que cualquier hijo termine
         wait(&returnCode);  // Esperar a que cualquier hijo termine
     }
     else if (pid == 0) {  // Código EXCLUSIVO del proceso hijo
-        childMain();
+        lanzarDado();
     }
 
     exit(EXIT_SUCCESS);
